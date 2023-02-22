40b8d7664a777f8290c9d3afc217f1141a582507
CHEVRIER Titouan 42 <tchevrie@student.42.fr>
2023-02-22 18:43:29 +0100

    builtin exit

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,11 +6,18 @@
 /*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 19:18:27 by tchevrie          #+#    #+#             */
-/*   Updated: 2023/02/22 18:23:59 by tchevrie         ###   ########.fr       */
+/*   Updated: 2023/02/22 18:42:19 by tchevrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ftbuiltin_exit(t_env *environnement)
+{
+	closing_the_program(environnement);
+	RETURNVAL = 0;
+	exit(RETURNVAL);
+}
 
 int	main(int argc, char **argv, char *envp[])
 {
@@ -28,6 +35,7 @@ int	main(int argc, char **argv, char *envp[])
 		if (!line)
 			break ;
 		free(line);
+		ftbuiltin_exit(environnement);
 	}
 	closing_the_program(environnement);
 	return (RETURNVAL);
